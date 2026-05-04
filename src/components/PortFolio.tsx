22709f2e943d6c6515f1094e3dd8528f36fb27c2
Jorge Gamboa <jorgegamboa@192.168.1.6>
2026-05-04 10:03:27 -0500

    Working 04May 10 am

--- src/components/PortFolio.tsx
+++ src/components/PortFolio.tsx
@@ -1,9 +1,16 @@
+
+import FlowerComponent from "./FlowerComponent";
+
+// import TabletScene from "./TabletScene";
+
 export default function PortFolio() {
     return (
         <section className="bg-page relative h-screen w-full overflow-hidden text-[rgb(240,239,235)] transition-colors duration-300">
             <h2 className="text-4xl font-semibold tracking-tight text-[rgb(240,239,235)] sm:text-5xl md:text-6xl">
                 PortFolio
             </h2>
+            <FlowerComponent />
+
         </section>
     )
 }
